7da67d0a8fc24567e8e7dd84820f5cac82e79d31
V5778 (Course ID) <v5778@cam.ac.uk>
2015-08-13 15:49:38 +0100

    module1 photofilters. Pixels and RGBA

--- module1.js
+++ module1.js
@@ -31,9 +31,100 @@ function getRandomInt(min, max) {
 
 $(document).ready(function() {
     var img = new Image();
-    img.src = "img/cat.jpg";
+    img.src = "img/Not Venice.jpg";
+    makeMorePurple(img);
+    //brighten(img, 50);
+    //makeInvert(img, 50)
+//makeNoise(img);
+    //makeFunky(img);
 
-
-
-
+    /*var pixels = ImageUtils.getPixels(img);
+    var heightPixelsToHide = 100;
+    for(var i = 0; i < img.width * heightPixelsToHide * 4; i++) {
+        pixels.data[i] = 255;
+    }
+    ImageUtils.putPixels(pixels, img.width, img.height);*/
 });
+
+//function makeMoreBlue(img) {
+
+    //var pixels = ImageUtils.getPixels(img);
+    //var length = pixels.data.length;
+    //var data = pixels.data;
+
+    //for (var i = 0; i < length; i += 4) {
+      //  data[i+2] = data[i+2] + 200;
+    //}
+    //ImageUtils.putPixels(pixels, img.width, img.height);
+//}
+
+function makeMorePurple(img) {
+
+    var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < length; i += 4) {
+
+        data[i + 2] = data[i + 2] + 2;
+        data[i] = data[i] + 50;
+    }
+    ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
+function brighten(img, adjustment) {
+    var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < length; i += 4) {
+
+        data[i] = data[i] + adjustment;
+        data[i + 1] = data[i + 1] + adjustment;
+        data[i + 2] = data[i + 2] + adjustment;
+
+    }
+    ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
+function makeInvert(img, adjustment) {
+    var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < length; i += 4) {
+        data[i] = 255 - data[i];
+        data[i + 1] = 255 - data[i + 1];
+        data[i + 2] = 255 - data[i + 2];
+
+    }
+    ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
+function makeNoise(img, level) {
+    var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < length; i += 4) {
+var adjustment = getRandomInt(-50, 50);
+        data[i] = data[i] + adjustment;
+        data[i + 1] = data[i + 1] + adjustment;
+        data[i + 2] = data[i + 2] + adjustment;
+    }
+    ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
+
+function makeFunky(img) {
+    var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < length/2; i += 2) {
+        var temp = data[i];
+        data[i] = data[length - i];
+        data[length - i] = temp;
+    }
+    ImageUtils.putPixels(pixels, img.width, img.height);
+}
